d0f9b1b54f769f35670a3d5437e1b5c9cbe54ef4
风风 <qwe18173945756@163.com>
2025-03-29 13:43:46 +0800

    fix:修复了图标显示不出来的bug

--- vite.config.ts
+++ vite.config.ts
@@ -25,12 +25,7 @@ export default defineConfig({
     createSvgIconsPlugin({
       iconDirs:[path.resolve(process.cwd(),'src/assets/icon')],
       symbolId:'icon-[dir]-[name]',
-      // svgoOptions: {
-      //   plugins: [
-      //     { name: 'preset-default' }, // 压缩 SVG
-      //     { name: 'removeViewBox', active: false }, // 保留 viewBox   
-      //   ],
-      // },
+      
     }),
     tailwindcss(),
     visualizer({ open: true, gzipSize: true }), // 可视化打包分析
@@ -38,44 +33,14 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve (__dirname,"./src"),
-      '~': path.resolve(__dirname, './src/assets'), // 新增资产别名
     },
   },
-  build: {  //build 配置只在构建（生产）时生效
-    target: 'esnext', // 目标现代浏览器
-    minify: process.env.NODE_ENV === 'production' ? 'esbuild' : false, // 使用 esbuild 压缩
-    sourcemap: process.env.NODE_ENV === 'development',
+  build: {  
     rollupOptions: {
       external: [
         /src\/.*\/_tests_\/.*/,
         /src\/onlyTestCom\/.*/
       ], // 排除测试文件
-      output: {
-        manualChunks: {   // 分包
-          vendor: ['vue', 'pinia'], // 提取核心依赖
-          utils: ['highlight.js', 'markdown-it'], // 工具库单独分包
-        },
-        chunkFileNames: 'js/[name]-[hash].js', // 优化 chunk 文件名
-        assetFileNames: '[ext]/[name]-[hash].[ext]', // 静态资源命名
-      },
-      treeshake: {
-        moduleSideEffects: false, // 假设模块无副作用，抖得更狠
-        tryCatchDeoptimization: false, // 避免 try-catch 干扰
-      },
-    },
-    terserOptions: {
-      compress: {
-        drop_console: true, // 移除 console
-        pure_funcs: ['console.info'], // 移除特定日志
-      },
-    },
-  },
-  optimizeDeps: {   // optimizeDeps仅开发阶段执行
-    include: [ 'vue',"highlight.js","markdown-it","pinia","tailwindcss"], // 强制预构建常用库
-    exclude: ['marked'], // 排除不常用库
-    esbuildOptions: {
-      target: 'esnext', // 一致的目标
-      treeShaking: true, // 预构建也开 Tree Shaking
     },
   },
 });
